877bc4f04a6f29bb3c771884436b133ab64b3139
2lchain <murimimlvictor@gmail.com>
2025-02-04 21:05:05 +0300

    plan functions

--- apps/cycling.ts
+++ apps/cycling.ts
@@ -1,4 +1,4 @@
-import { Device, getStreamingCounts, StreamAndChannels, TCPDeinit, TCPFindDevices, TCPInit, USBDeInit, USBFindDevices, USBInit } from "../haptica-asphodel-js";
+import { ChannelDecoder, ChannelInfo, createDeviceDecoder, Device, DeviceDecoder, getStreamingCounts, StreamAndChannels, StreamInfo, TCPDeinit, TCPFindDevices, TCPInit, UnitFormatter, USBDeInit, USBFindDevices, USBInit } from "../haptica-asphodel-js";
 
 function init() {
     USBInit()
@@ -10,15 +10,108 @@ function deinit() {
     TCPDeinit()
 }
 
-
-function checkAllConnectedReceivers() {
-    let usb_devices = USBFindDevices(50);
-    let tcp_devices = TCPFindDevices(50);
-    return {
-        tcp_devices: tcp_devices,
-        usb_devices: usb_devices
-    }
+type ChannelClosure = {
+    unit_formatter: UnitFormatter,
+    counter_time_scale: number,
+    sample_time_scale: number
 }
+
+type DeviceInfo = {
+    decoder: DeviceDecoder,
+    stream_count: number,
+    info_array: StreamAndChannels[],
+    serial_number: string
+}
+
+function createChannelClosure(
+    serial_number: string,
+    stream_info: StreamInfo,
+    channel_info: ChannelInfo,
+    channel_decoder: ChannelDecoder) {
+    let strinfo = stream_info.getInfo();
+    let chinfo = channel_info.getInfo();
+
+    let channel_closure: ChannelClosure = {
+        unit_formatter: new UnitFormatter(chinfo.unit_type, chinfo.minimum, chinfo.maximum, chinfo.resolution, true),
+        counter_time_scale: 0.0,
+        sample_time_scale: 0.0
+    }
+
+    if (strinfo.rate != 0.0 && channel_decoder.getSamples() != 0) {
+        channel_closure.counter_time_scale = 1 / (strinfo.rate)
+        channel_closure.sample_time_scale = channel_closure.counter_time_scale / channel_decoder.getSamples()
+    }
+
+    return channel_closure;
+}
+
+function createDeviceInfo(device: Device): DeviceInfo {
+    let stream_count = device.getStreamCount();
+    let stream_infos: StreamAndChannels[] = [];
+    for (let i = 0; i < stream_count.count; i++) {
+        let stream = device.getStream(i);
+        let channels: ChannelInfo[] = []
+        let stream_info = stream.getInfo();
+
+        if (stream_info.channel_count == 0) {
+            throw new Error(`Error: stream ${i} has 0 channels`);
+        }
+
+        for (let j = 0; j < stream_info.channel_count; j++) {
+            let channel_index = stream_info.channel_index_list[j];
+            let channel = device.getChannel(channel_index);
+            channels.push(channel)
+        }
+        stream_infos.push(new StreamAndChannels(i, stream, channels))
+    }
+
+    let decoder = createDeviceDecoder(stream_infos, stream_count.filler_bits, stream_count.id_bits);
+
+    let serial_number = device.getSerialNumber();
+
+    decoder.setUnknownIDCallback((id) => {
+        console.log(`Unknown stream id ${id} on ${serial_number}`)
+    })
+
+    let decs = decoder.getDecoders();
+
+
+    decs.forEach((dec, i) => {
+        let stream_info = stream_infos[i].getStreamInfo();
+
+        dec.setLostPacketCallback((current, last) => {
+            console.log(`Lost ${current - last - 1} from ${serial_number} stream ${i} `)
+        })
+
+        dec.getDecoders().forEach((channel_decoder, j) => {
+            let channel_info = stream_infos[i].getChannelInfos()[j];
+            let channel_closure = createChannelClosure(serial_number, stream_info, channel_info, channel_decoder)
+            channel_decoder.setConversionFactor(channel_closure.unit_formatter.getConversionScale(), channel_closure.unit_formatter.getConversionOffset())
+            channel_decoder.setDecodeCallback((counter, data, samples, subchannels) => {
+                for (let sample = 0; sample < samples; sample++) {
+                    if (channel_closure.counter_time_scale == 0) {
+                        console.log("channel_closure.counter_time_scale == 0", counter);
+                    } else {
+                        let time = counter * channel_closure.counter_time_scale + sample * channel_closure.sample_time_scale;
+                        console.log("channel_closure.counter_time_scale != 0", time)
+                    }
+                    for (let sub = 0; sub < subchannels; sub++) {
+                        console.log(channel_closure.unit_formatter.FormatBare(data[sample * subchannels + sub]))
+                    }
+                }
+            })
+        })
+    })
+
+    return {
+        stream_count: stream_count.count,
+        serial_number: device.getSerialNumber(),
+        decoder: decoder,
+        info_array: stream_infos
+    }
+
+}
+
 
 async function checkSensorsConnected(device: Device) {
     let remote_devices: Device[] = [];
@@ -42,7 +135,6 @@ async function checkSensorsConnected(device: Device) {
                         console.error(e);
                         return
                     }
-
                     remote_devices.push(remote);
                 })
                 resolve(true)
@@ -53,32 +145,61 @@ async function checkSensorsConnected(device: Device) {
 }
 
 
-function aquireData(device: Device) {
+async function aquireData(device: Device, time: number) {
+    var device_info = createDeviceInfo(device);
+
+
     let response_time = 0.100; // 100 milliseconds
     let buffer_time = 0.500; // 500 milliseconds
     let timeout = 1000; // 1000 milliseconds
+    let streaming_counts = getStreamingCounts(device_info.info_array, response_time, buffer_time, timeout)
 
-    let stream_and_channel = new StreamAndChannels()
-
-    let streaming_counts = getStreamingCounts(device_info_array[i].info_array, response_time, buffer_time, timeout)
+    console.log(`Enabling ${device_info.stream_count} streams from ${device_info.serial_number}`)
 
     console.log("Transfer count: ", streaming_counts.transfer_count);
 
-    devices[i].startStreamingPackets(streaming_counts.packet_count,
+    device.startStreamingPackets(streaming_counts.packet_count,
         streaming_counts.transfer_count,
         streaming_counts.timeout, (status, stream_data, packet_size, packet_count) => {
             if (status == 0) {
                 for (let packet = 0; packet < packet_count; packet++) {
-                    device_info_array[i].decoder.decode(stream_data.slice(packet * packet_size))
+                    device_info.decoder.decode(stream_data.slice(packet * packet_size))
                 }
             } else {
                 console.log(`Bad status ${status} in streaming packet callback`);
             }
         });
 
-        for (let j = 0; j < device_info_array[i].stream_count; j++) {
-            devices[i].enableStream(j, true);
-        }
+    for (let j = 0; j < device_info.stream_count; j++) {
+        device.enableStream(j, true);
+    }
+
+
+    await new Promise((resolve)=>{
+        setTimeout(()=>{
+            for(;;) {
+                device.poll(1000)
+            }
+        }, time)
+    })
+
+
+    console.log(`Disabling ${device_info.stream_count} streams from ${device_info.serial_number}`)
+    for (let j = 0; j < device_info.stream_count; j++) {
+        device.enableStream(j, false);
+    }
+
+    device.stopStreamingPackets();
+    device.poll(10);
+}
+
+function checkAllConnectedReceivers() {
+    let usb_devices = USBFindDevices(50);
+    let tcp_devices = TCPFindDevices(50);
+    return {
+        tcp_devices: tcp_devices,
+        usb_devices: usb_devices
+    }
 }
 
 
